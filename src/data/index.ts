a9dbeacfe0ad20ea1aaf026ac6b5c3029bdc9c6e
ArthurFkOne <arturohrh@gmail.com>
2024-01-17 10:38:42 -0500

    feat: header

---
+++ src/data/index.ts
@@ -0,0 +1,16 @@
+
+
+export const data = {
+    carousel: [
+        {
+            background: "/carousel/carro-1.svg",
+            title: 'Servicio de transporte',
+            subtitle: 'En CANCÚN',
+            button: {
+                type: 1 ,
+                text: 'Ver Destinos',
+                to:  '/'
+            }
+        }
+    ]
+}
