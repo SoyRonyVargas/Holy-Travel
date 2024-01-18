ffb707134401509ebbb4985e143857155a224860
Jonathan Valdes Santiago <jonathanvaldesantiago98@gmail.com>
2024-01-18 12:49:37 -0500

    feat: buscador

--- src/components/Buscador.tsx
+++ src/components/Buscador.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Buscador = () => {
   return (
-    <div>Buscador</div>
+    <div className='py-10 px-10 bg-custom '>
+      <div className="max-w-[1240px] mx-auto flex justify-between w-full border-black">
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+          <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+        </div>
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+          <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+        </div>
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+          <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Carousel2.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carousel2 = () => {
+  return (
+    <div className='py-10 px-10 bg-blue-800'>
+      
+    </div>
+  )
+}
+
+export default Carousel2
